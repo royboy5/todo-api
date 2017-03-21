d6b77c717004bd3eb40ad0e24186985573a1ec30
Roy Eng <roye19@gmail.com>
2017-03-21 16:47:03 -0400

    Added getenv module and updated config.js to use ENV vars

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 'use strict'
 
+//const config = require('./config')
 const express = require('express')
 const app = express()
 
---
+++ src/config.js
@@ -0,0 +1,19 @@
+'use strict'
+
+const getenv = require('getenv');
+
+// Enter all config options here
+
+module.exports = {
+
+    logging: {
+        level: getenv('LOG_LEVEL', 'debug')
+    },
+
+    db: {
+        mongodb: {
+            uri: getenv('DB_URI')
+        }
+    }
+
+}

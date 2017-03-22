4bbd0b21deaae17a20ace8e35a8e4b824b82fba2
Roy Eng <roye19@gmail.com>
2017-03-22 11:00:41 -0400

    Added Moment module to app

--- src/app.js
+++ src/app.js
@@ -4,13 +4,14 @@
 const path = require('path')
 const express = require('express')
 const app = express()
+const moment = require('moment')
 const utils = require('require-all')(path.join(__dirname, 'utils'))
 const log = utils.logger
 
-log.info('App Starting...')
+log.info(`${moment().format('MM-DD-YYYY HH:mm:ss')} - App Starting...`)
 
 app.get('/', (req, res) => {
-    log.info('Accessed /')
+    log.info(`${moment().format('MM-DD-YYYY HH:mm:ss')} - Accessed /`)
     res.send('Hello');
 })
 

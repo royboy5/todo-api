2221b8bddddb6090a529171c217bb21cd3af6ab7
Eng <royeng@EMERALDEXPO.NET>
2017-05-26 14:50:43 -0400

    Updated Users

--- src/routes/user.js
+++ src/routes/user.js
@@ -8,7 +8,7 @@ const userController = require('../controllers/userController')
 router.get('/', userController.user_list)
 router.get('/:user', userController.user_get)
 router.post('/', userController.user_create_post)
-router.delete('/:user', userController.user_delete_post)
 router.patch('/:user', userController.user_update_post)
+router.delete('/:user', userController.user_delete_post)
 
 module.exports = router
